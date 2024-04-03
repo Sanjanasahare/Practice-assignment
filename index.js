e465af552c0d703108535e27e3ecef788dd4d630
Sanjanasahare <sanjanasahare2812@gmail.com>
2024-04-03 16:30:02 +0530

    updated

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 import express from 'express';
 
 const app=express();
+app.use(express.json());
 
 const PORT=5000;
 
-app.get("/burger",(req,res)=> {
+app.get("/order/food",(req,res)=> {
+    const {menu,price,quantity}=req.query;
+
+    const totalprice= parseInt(price) * parseInt(quantity);
+
     res.json({
-        name:"Veg Burger",
-        price:89
+        message:`You have ordered ${quantity} ${menu}`,
+        bill:`Your total bill is ${totalprice}`
     })
+})
+//param
+app.get("/food:type",(req,res)=>{
+
+    const {type} = req.params;
+
+    if(type=="veg"){
+        return res.json({
+            message: "You have ordered veg food"
+        })
+    }
+    else{
+        return res.json({
+            message:"You have ordered non-veg food"
+        })
+    }
+})
+
+app.get("/orders",(req,res)=>{
+    const {user,country}=req.headers;
+
+    res.json({
+        details:`you are ${user} from ${country}`
+    })
+
 })
 
 app.get("/pizza",(req,res)=> {
@@ -22,6 +52,16 @@ app.get("/pasta",(req,res)=> {
     res.json({
         name:"white sauce Pasta",
         price:110
+    })
+})
+
+
+
+app.post("/user",(req, res)=>{
+    const {name, age} = req.body;
+
+    res.json({
+        message:`Hello ${name} ,you are ${age} year old`
     })
 })
 
